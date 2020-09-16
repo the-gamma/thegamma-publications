153f25b746c16cc0cc4b384cfcdb728a4a0cf14a
Tomas Petricek <tomas@tomasp.net>
2020-09-17 00:25:11 +0100

    Finalizing submission

--- wip-design/studies/analysis.fsx
+++ wip-design/studies/analysis.fsx
@@ -1,3 +1,19 @@
 #load "C:/Temp/nuget/packages/FsLab/FsLab.fsx"
 open Deedle
 
+System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+// Drop personal data (used to link answers to participants)
+let dfn = Frame.ReadCsv("questionnaire.csv")
+dfn.DropColumn("Username")
+dfn.SaveCsv("questionnaire.csv", includeRowKeys=false)
+
+// Convert to correct Likert scale (1=strongly disagree, 5=strongly agree)
+let df = Frame.ReadCsv("questionnaire.csv")
+let dfl = df |> Frame.mapValues (fun (n:int) -> 6-n)
+
+let results = frame [
+  "Mean" => (dfl |> Stats.mean)
+  "Sdv" => (dfl |> Stats.stdDev) ]
+let rounded = results |> Frame.mapValues (fun (v:float) -> System.Math.Round(v, 2) )
+rounded.SaveCsv("results.csv", keyNames=["Question"])
